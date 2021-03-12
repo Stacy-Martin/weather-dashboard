a548417064383bcb34cfe1ed731feb3959a42023
Stacy Martin <sbrown1031@gmail.com>
2021-03-12 01:59:34 -0600

    pseudo js

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,5 @@
+init()
+
 // display current date function
 const dateDisplayEl = $('#date-display');
 const todaysDate = moment().format('MMM DD, YYYY');
@@ -6,8 +8,48 @@ function displayDate() {
 }
 displayDate()
 
+const searchBar = document.getElementById('search-bar');
+const searchBtn = document.getElementById('search-btn');
+
+// https://openweathermap.org/api
+
+// create searchFx for cities searched by user
+// trim city
+// get current weather for that city = call 1
+// get five day forecast for that city = call 2
+
+
+$('#search-btn').click(searchFx);
+
+
+// create currentWeather function
+// let API url / fetch / catch
+// display weather
+// ? error alerts
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={ded1e9b5e5ea66606148d61b8d281024}
+
+
+// create fiveDay function
+// let API url / fetch / catch
+// display weather
+// ? error alerts
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={ded1e9b5e5ea66606148d61b8d281024}
 
 
 
-// ded1e9b5e5ea66606148d61b8d281024
-// https://home.openweathermap.org/api_keys
+// add fetched weather data to appropriate spot in HTML for currentWeather
+// use of append and addClass methods
+// let statements to addClass (like in create table in calendar)
+
+
+
+// add fetched weather data to appropriate spot in HTML for fiveDay
+// use of append and addClass methods
+// let statements to addClass (like in create table in calendar)
+// have to do this for each of the five day col's
+// create if/then statement for weather icons from here:
+// https://openweathermap.org/weather-conditions#Icon-list
+
+
+
+// local storage  ???
