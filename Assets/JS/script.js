f472eb36c33809366a80f256a0440fa777bfddf8
Stacy Martin <sbrown1031@gmail.com>
2021-03-15 11:29:37 -0500

    js work

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,105 +1,120 @@
-
-
 // display current date function
-const dateDisplayEl = $('#date-display');
-const todaysDate = moment().format('MMM DD, YYYY');
+const dateDisplayEl = $("#date-display");
+const todaysDate = moment().format("MMM DD, YYYY");
 function displayDate() {
-    dateDisplayEl.text(todaysDate);
+  dateDisplayEl.text(todaysDate);
 }
-displayDate()
+displayDate();
 
-const searchBar = document.getElementById('search-bar');
-const searchBtn = document.getElementById('search-btn');
+const searchBar = document.getElementById("search-bar");
+const searchBtn = document.getElementById("search-btn");
 
-// https://openweathermap.org/api
-
-// create searchFx for cities searched by user
-// trim city
-// get current weather for that city = call 1
-// get five day forecast for that city = call 2
-
-$(document).ready(function(){
-     $('#search-btn').click(searchFx);
-}
-   
-);
+$(document).ready(function () {
+  $("#search-btn").click(searchFx);
+});
 
 
-//let means the contents of the variable may change
-//const is constant, cannot change
-let searchFx = function (){
+let searchFx = function () {
+  //this retrieves the value of our input for the city
+  let city = $("#search-bar").val();
 
-    //this retrieves the value of our input for the city
-    let city = $('#search-bar').val();
-    console.log(city);
 
-    //now we want to get our api calls using the parsed city 
-    //first let's get the 5 day forecast
-    fiveDay(city);
+  //Get our api calls using the parsed city, and also get the 5 day forecast
+  currentWeather(city)
+  fiveDay(city);
+};
 
-}
-// create currentWeather function
-// let API url / fetch / catch
-// display weather
-// ? error alerts
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={ded1e9b5e5ea66606148d61b8d281024}
+//  create function for today's weather
+let currentWeather = function (city) {
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ded1e9b5e5ea66606148d61b8d281024&units=imperial`;
 
-// should i use let or const here ?
-let currentWeather = function(city){
-    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ded1e9b5e5ea66606148d61b8d281024`
-
-    fetch(apiURL)
-        .then(function (response){
-            if (response.ok){
-
-            }
-        });
-}
-
-// create fiveDay function
-// let API url / fetch / catch
-// display weather
-// ? error alerts
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={ded1e9b5e5ea66606148d61b8d281024}
-
-let fiveDay = function (city) {
-    let apiURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=ded1e9b5e5ea66606148d61b8d281024`;
-    //res is response
-    fetch(apiURL)
-        .then(function (res){
-            if (res.ok){
-            //this sends a json object to our next promise aka the then
-            return res.json().then((data)=>{
-                //arrow function is the same as me writing function
-                //this converts our data from a string into a JSON object / aka we can now use javascript on it
-                //parse
-            console.log(data)
-            console.log(data.main.temp) 
-        });
-    } else {
-        alert('Error: ' + response.statusText);
+  fetch(apiURL)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
       }
     })
+    .then(function (data) {
+            console.log(data);
+      const currentWeatherCard = $("#currrentWeatherCard");
+
+      //this empties the contents of the row
+      currentWeatherCard.empty();
+
+      const resDay = data;
+      const currentDayCard = $("<div>").addClass("card todays-weather"); 
+      const day = $("<h4>").addClass("card-title");
+      const weatherIcon = $("<img>");
+      const temperatureCard = $("<p>").addClass("card-text");
+      const humidityCard = $("<p>").addClass("card-text");
+      //the ${} is a template literal, here we are taking in the icon id in the url
+      //from the api call we got for the day
+      weatherIcon.attr(
+        "src",
+        `http://openweathermap.org/img/w/${resDay.weather[0].icon}.png`
+      );
+
+      temperatureCard.text(resDay.main.temp_max);
+      humidityCard.text(resDay.main.humidity);
+
+      currentDayCard.append(day);
+      currentDayCard.append(weatherIcon);
+      currentDayCard.append(temperatureCard);
+      currentDayCard.append(humidityCard);
+
+
+    //   fiveDayCard.append(cardContainer);
+      currentWeatherCard.append(currentDayCard);
+    })
     .catch(function (error) {
-      alert('Unable to connect');
+      console.log(error);
     });
 };
 
 
+// create function for five day forecast
+let fiveDay = function (city) {
+  let apiURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=ded1e9b5e5ea66606148d61b8d281024&units=imperial`;
 
-// add fetched weather data to appropriate spot in HTML for currentWeather
-// use of append and addClass methods
-// let statements to addClass (like in create table in calendar)
+  fetch(apiURL)
+    .then(function (res) {
+      if (res.ok) {
+        //this sends a json object to the next promise 
+        return res.json().then((data) => {
 
+          // 8 time increments per day, start in middle of the day at i=3 then add 8 5x
+          const main5dayRow = $("#5-day-forecast");
+          //this empties the contents of our row
+          main5dayRow.empty();
+          for (let i = 3; i < data.list.length; i = i + 8) {
 
+            const resDay = data.list[i];
+            const fiveDayCard = $("<div>").addClass("col five-day-forecast"); 
+            const cardContainer = $("<div>").addClass("card card-body");
+            const day = $("<h4>").addClass("card-title");
+            const weatherIcon = $("<img>");
+            const temperatureCard = $("<p>").addClass("card-text");
+            const humidityCard = $("<p>").addClass("card-text");
+            day.text(resDay.dt_txt.split(" ")[0]);
+            weatherIcon.attr(
+              "src",`http://openweathermap.org/img/w/${resDay.weather[0].icon}.png`);
+            temperatureCard.text(resDay.main.temp_max);
+            humidityCard.text(resDay.main.humidity);
 
-// add fetched weather data to appropriate spot in HTML for fiveDay
-// use of append and addClass methods
-// let statements to addClass (like in create table in calendar)
-// have to do this for each of the five day col's
-// create if/then statement for weather icons from here:
-// https://openweathermap.org/weather-conditions#Icon-list
+            cardContainer.append(day);
+            cardContainer.append(weatherIcon);
+            cardContainer.append(temperatureCard);
+            cardContainer.append(humidityCard);
 
-// init()
-
-// local storage  ??
+            fiveDayCard.append(cardContainer);
+            main5dayRow.append(fiveDayCard);
+          }
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
